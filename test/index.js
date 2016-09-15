c3e4c29cafd32bedea27135f7f9cf33382c4e4c3
Yoel Monzon <yoelfme@hotmail.com>
2016-09-15 17:37:21 -0600

    [add] test for plugin

---
+++ test/index.js
@@ -0,0 +1,135 @@
+'use strict'
+
+const Lab = require('lab')
+const Code = require('code')
+const Hapi = require('hapi')
+const Vcr = require('vcrecorder')
+const Plugin = require('../lib')
+
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const expect = Code.expect
+
+const internals = {}
+let server = null
+
+lab.beforeEach(done => {
+  server = new Hapi.Server()
+  server.connection()
+  done()
+})
+
+const validAccessToken = process.env.VALID_ACCESS_TOKEN
+const invalidAccessToken = process.env.INVALID_ACCESS_TOKEN
+const oauthServerHost = process.env.OAUTH_HOST
+const oauthClientId = process.env.OAUTH_CLIENT_ID
+const oauthClientSecret = process.env.OAUTH_CLIENT_SECRET
+
+describe('hapi-oauth2-access-token', () => {
+
+  it('register well, :)', done => {
+    server.register({
+      register: Plugin,
+      options: {
+        host: 'host',
+        clientId: 'clientId',
+        clientSecret: 'clientSecret'
+      }
+    }, err => {
+      expect(err).to.not.exist()
+      server.stop()
+      done()
+    })
+  })
+
+  it('register bad :(', done => {
+
+    const register = Plugin.register
+    Plugin.register = (server, options, next) => {
+
+      return next(new Error('Fail Registration'))
+    }
+
+    Plugin.register.attributes = {
+      name: 'fake plugin'
+    }
+
+    server.register(Plugin, err => {
+
+      Plugin.register = register
+      expect(err).to.exist()
+      server.stop()
+      done()
+    })
+  })
+
+  it('get resource with valid token', done => {
+    server.register({
+      register: Plugin,
+      options: {
+        host: oauthServerHost,
+        clientId: oauthClientId,
+        clientSecret: oauthClientSecret
+      }
+    }, err => {
+      expect(err).to.not.exist()
+
+      server.route({
+        method: 'GET',
+        path: '/',
+        config: {
+          auth: 'bearer',
+          handler:  (request, reply) => {
+            reply('private resource')
+          }
+        }
+      })
+
+      Vcr.insert('get-resource-valid-token')
+      server.inject({ method: 'GET', url: '/',  headers: {authorization: `Bearer ${validAccessToken}`}}, res => {
+        expect(res.statusCode).to.equal(200)
+        expect(res.result).to.equal('private resource')
+        server.stop()
+
+        Vcr.eject(rec => {
+          done()
+        })
+      })
+    })
+  })
+
+  it('get resource with invalid token, focus', done => {
+    server.register({
+      register: Plugin,
+      options: {
+        host: oauthServerHost,
+        clientId: oauthClientId,
+        clientSecret: oauthClientSecret
+      }
+    }, err => {
+      expect(err).to.not.exist()
+
+      server.route({
+        method: 'GET',
+        path: '/',
+        config: {
+          auth: 'bearer',
+          handler:  (request, reply) => {
+            reply('private resource')
+          }
+        }
+      })
+
+      Vcr.insert('get-resource-invalid-token')
+      server.inject({ method: 'GET', url: '/',  headers: {authorization: `Bearer ${invalidAccessToken}`}}, res => {
+        expect(res.statusCode).to.equal(401)
+        expect(res.result.message).to.equal('Invalid access token')
+        server.stop()
+        Vcr.eject(rec => {
+          done()
+        })
+      })
+    })
+  })
+})
